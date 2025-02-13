6453c5d52b69da5d76206c3c6da529851112747d
bderalq <balqallaf12@gmail.com>
2025-02-13 12:19:12 +0300

    FINAL

--- objectIteration.js
+++ objectIteration.js
@@ -31,32 +31,23 @@ const cities = [
   },
 ];
 
-// 1) Write a `getCityPopulation` function that accepts `city` object argument, return the population of that city
 function getCityPopulation(city) {
-  // write your code here...
+  return city.population;
 }
-// console.log(getCityPopulation(cities[0])); // Outputs: 8398748
-
-// 2) Write a `isPopulationAboveThreshold` function that accepts the following arguments `city` object and `threshold` number, return true if the city's population is greater than or equal to the threshold, otherwise returns false
 function isPopulationAboveThreshold(city, threshold) {
-  // write your code here...
+  if(city.population > threshold) {
+    return true;
+  } return false;
 }
-// console.log(isPopulationAboveThreshold(cities[1], 2000000)); // Outputs: true
 
-// 3) Write a `addCity` function that accepts the following argument, an array of city object `cities` and the properties of a city (id, name, population, and country).
-// Create a new city object and add it to the end of the array.
-// Return the updated array of cities
 function addCity(cities, id, name, population, country) {
-  // write your code here...
+  const newCity = {id, name, population, country, };
+  cities.push(newCity);
+  return cities;
 }
-// console.log(addCity(cities, 306,  "Vancouver", 1200000,  "Canada" ));
-
-// 4) Write a `countCitiesInCountry` function that accepts the following arguments, an array of city objects `cities` and `country` string, return the number of cities in the specified country
 function countCitiesInCountry(cities, country) {
-  // write your code here...
+  return cities.filter(c => c.country == country).length;
 }
-// console.log(countCitiesInCountry(cities, "France")); // Outputs: 1
-
 module.exports = {
   getCityPopulation,
   isPopulationAboveThreshold,
